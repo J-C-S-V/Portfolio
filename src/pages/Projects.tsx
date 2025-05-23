f8dde926e0acb23e41d214a543a4041619d5d6ca
Juan Sanchez <juansan1993@hotmail.com>
2025-05-23 15:55:11 -0500

    Add projects component

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,52 @@
 import { Link } from 'react-router-dom';
+import styles from './Projects.module.css';
+import { BentoItem } from '../components/BentoGrid';
+import projects from '../data/projects_all.json';
 
 export const Projects = () => {
-    return (
-        <div>
-            <h1>Projects</h1>
-            <Link to="/">Home</Link>
+
+  return (
+    <main className={styles.projects}>
+      <h1 className={styles['projects__title']}>Projects</h1>
+      <div className={styles['projects__content']}>
+        <div className={styles['projects__grid']}>
+          {projects.map((project, index) => (
+            <BentoItem 
+              key={index}
+              className={`bento-item-content-${index}`}
+            >
+              <div className="project-image">
+                <img 
+                  src={project.image} 
+                  alt={project.title} 
+                  className="project-image"
+                />
+              </div>
+              <h3 className="project-title">
+                {project.title}
+              </h3>
+              <p className="project-description">
+                {project.description}
+              </p>
+              <div className="project-tags">
+                {project.tags.map((tag, i) => (
+                  <span key={i} className="project-tag">
+                    {tag}
+                  </span>
+                ))}
+              <a href={project.link} target="_blank" className="project-tag-link">View Project
+              </a>
+              <a href={project.github} target="_blank" className="project-tag-link">GitHub
+              </a>
+              </div>
+            </BentoItem>
+          ))}
         </div>
-    );
-}
+      </div>
+
+      <Link to="/" className={styles['projects__link']}>
+        Home
+      </Link>
+    </main>
+  );
+};
